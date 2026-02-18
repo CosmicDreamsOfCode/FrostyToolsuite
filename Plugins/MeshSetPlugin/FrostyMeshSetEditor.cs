8cf742e97b6750629f9f6290929ab9ad0a730c3b
August <faintlightswitch@gmail.com>
2026-02-18 18:25:36 +0000

    add triangulation error

--- Plugins/MeshSetPlugin/FrostyMeshSetEditor.cs
+++ Plugins/MeshSetPlugin/FrostyMeshSetEditor.cs
@@ -1600,6 +1600,14 @@ namespace MeshSetPlugin
         {
         }
     }
+
+    public class FBXImportNotTriangulatedException : Exception
+    {
+        public FBXImportNotTriangulatedException(string sectionName)
+            : base(string.Format("Encountered face that wasn't triangulated on imported object {0}. Make sure to triangulate your mesh before importing!", sectionName))
+        {
+        }
+    }
     #endregion
 
     public class FBXImporter
@@ -2466,6 +2474,11 @@ namespace MeshSetPlugin
 
                 for (int i = 0; i < fmesh.PolygonCount; i++)
                 {
+                    //check if polygon is comprised of more than 3 vertices
+                    if (fmesh.GetPolygonSize(i) > 3)
+                    {
+                        throw new FBXImportNotTriangulatedException(sectionNode.Name);
+                    }
                     for (int j = 0; j < 3; j++)
                     {
                         //int index = (i * 3) + j;
